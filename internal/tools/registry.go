7cb8a0fe2e29a5ca6a8d48a244998048fb8d6283
bryantjandra <tjandra.benedict@gmail.com>
2026-09-14 16:10:34 +0800

    feat: add tool registry

---
+++ internal/tools/registry.go
@@ -0,0 +1,38 @@
+package tools
+
+import "github.com/bryantjandra/friday/internal/llm"
+
+type Registry struct {
+	ToolMap map[string]Tool
+}
+
+func NewRegistry() *Registry {
+	return &Registry{
+		ToolMap: make(map[string]Tool),
+	}
+}
+
+func (r *Registry) RegisterTool(tool Tool) {
+	r.ToolMap[tool.Name()] = tool
+}
+
+func (r *Registry) GetTool(name string) (Tool, bool) {
+	tool, ok := r.ToolMap[name]
+	if !ok {
+		return nil, false
+	}
+	return tool, true
+}
+
+func (r *Registry) Definitions() []llm.ToolDef {
+	var toolDefs []llm.ToolDef
+	for _, tool := range r.ToolMap {
+		toolDef := llm.ToolDef{
+			Name:        tool.Name(),
+			Description: tool.Description(),
+			InputSchema: tool.InputSchema(),
+		}
+		toolDefs = append(toolDefs, toolDef)
+	}
+	return toolDefs
+}
